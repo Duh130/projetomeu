44e0a0cafed3a1b6948c638b066bea3e740c838b
duh <duka13505@gmail.com>
2024-04-29 09:21:36 -0300

    atu

---
+++ projeto filme/src/scripts/main.js
@@ -0,0 +1,66 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
+
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            hideHeaderElements();
+        } else{
+            showHeaderElements();
+        }
+    })
+
+    //Programação das abas
+    for (let i=0; i< buttons.length; i++){
+        buttons[i].addEventListener("click", function(botao){
+            const activeTab = botao.target.dataset.tabButton;
+            const tab = document.querySelector(`[data-tab-id=${activeTab}]`);
+            hideAllTabs();
+            tab.classList.add("shows__list--is-active");
+            removeActiveButton();
+            botao.target.classList.add("shows__tabs__button--is-active");
+        })
+    }
+
+    // FAQ Accordion 
+    for (let i = 0; i < questions.length; i++){
+        questions[i].addEventListener("click", abreFechaResposta);
+    }
+
+})
+
+function abreFechaResposta(elemento){
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+    elementoPai.classList.toggle(classe);
+}
+
+function removeActiveButton(){
+    const buttons = document.querySelectorAll('[data-tab-button]');
+    for (let i = 0; i< buttons.length; i++){
+        buttons[i].classList.remove("shows__tabs__button--is-active");
+    }
+}
+
+function hideAllTabs(){
+    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+
+    for (let i = 0; i< tabsContainer.length; i++){
+        tabsContainer[i].classList.remove("shows__list--is-active");
+    }
+}
+
+function hideHeaderElements() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function showHeaderElements() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
